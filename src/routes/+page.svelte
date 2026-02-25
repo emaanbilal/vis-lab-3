e96fbd665ed872ce336a1491535f8de1c439e744
emaanbilal <emaanbilal@gmail.com>
2026-02-25 14:22:04 -0500

    migrating content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <!DOCTYPE html>
+    <html lang="en">
+        <link rel="stylesheet" href="style.css">
+    <head>
+        <nav>
+            <a class="current" href=".">Home</a>
+            <a href="projects">Projects</a>
+            <a href="contact">Contact</a>
+            <a href="resume">Resume</a>
+            <a href="https://www.github.com/emaanbilal" target="_blank">GitHub</a>
+        </nav>
+        </head>
+    <body>
+        <title>Emaan Bilal Khan: Personal site and portfolio</title>
+        <h1>Emaan Bilal Khan</h1>
+        <p>I am a Technology and Policy Graduate Student at MIT, where I research on AI safety and policy.</p>
+        <img src="images/bg.jpg" alt="Photo of Lahore, Pakistan (my hometown).">
+    </body>
+    </html>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,3 +1,82 @@
 <svelte:head>
   <title>Projects</title>
 </svelte:head>
+
+<!DOCTYPE html>
+<html lang="en">
+    <link rel="stylesheet" href="../style.css">
+<head>
+  <nav>
+      <a href=".">Home</a>
+      <a class="current" href="projects">Projects</a>
+      <a href="contact">Contact</a>
+      <a href="resume">Resume</a>
+      <a href="https://github.com/emaanbilal" target="_blank">GitHub</a>
+  </nav>
+</head>
+<body>
+    <h1>Projects</h1>
+    <div class="projects">
+        <article>
+            <h2>Exploratory Data Analysis of Corporate Ownership and Evictions in Boston</h2>
+            <img src="https://png.pngtree.com/png-vector/20240723/ourmid/pngtree-an-isometric-view-of-a-city-skyline-with-tall-buildings-and-png-image_13216416.png" width="80%" alt="">
+            <a href="../../A2.html">View Project</a>
+        </article>
+        <article>
+            <h2>Consectetur adipiscing elit</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+        </article>
+        <article>
+            <h2>Sed do eiusmod tempor</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </article>
+        <article>
+            <h2>Incididunt ut labore et</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+        </article>
+        <article>
+            <h2>Dolore magna aliqua</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </article>
+        <article>
+            <h2>Ut enim ad minim</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+        </article>
+        <article>
+            <h2>Veniam quis nostrud exercitation</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </article>
+        <article>
+            <h2>Ullamco laboris nisi</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        </article>
+        <article>
+            <h2>Aliquip ex ea commodo</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </article>
+        <article>
+            <h2>Consequat duis aute</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+        </article>
+        <article>
+            <h2>Irure dolor in reprehenderit</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </article>
+        <article>
+            <h2>Voluptate velit esse</h2>
+            <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+        </article>
+    </div>
+</body>
+</html>
